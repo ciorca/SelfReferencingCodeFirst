edcefcb0584b0f3b7eed4774b111edf7930bedfb
ciorca <ciorca.gaby@gmail.com>
2022-06-09 15:06:10 +0300

    Add project files.

---
+++ SelfReferencingCodeFirst/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SelfReferencingCodeFirst
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public int? ManagerID { get; set; }
+        public Employee Manager { get; set; }
+
+    }
+}
